944567dca232f4bb9a03acd7880b591f0b434bcf
Gladson Antony <GladsonAntony91@gmail.com>
2019-07-14 12:13:26 +0530

    First Push

---
+++ src/test/java/tests/Test1_ABTesting.java
@@ -0,0 +1,26 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1_ABTesting
+{
+    @Test
+    public void test1_ABTesting() throws Exception
+    {
+        WebDriverManager.chromedriver().arch32().setup();
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.get("http://the-internet.herokuapp.com/");
+        System.out.println("Page Title --> "+webDriver.getTitle());
+        webDriver.findElement(By.linkText("A/B Testing")).click();
+        System.out.println("\nPage Title -->"+webDriver.getTitle());
+        System.out.println("Page Header --> "+webDriver.findElement(By.xpath("//h3")).getText());
+        System.out.println("Page Content: \n" +webDriver.findElement(By.xpath("//p")).getText());
+        webDriver.quit();
+    }
+
+}
---
+++ src/test/java/tests/Test2_AddRemoveElements.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Test2_AddRemoveElements
+{
+    @Test
+    public void test2_Add_Remove_Elements() throws Exception
+    {
+
+    }
+}
