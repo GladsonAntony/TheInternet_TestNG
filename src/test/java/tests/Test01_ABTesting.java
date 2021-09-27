8bf357d13b3809d93c90a2d37e663dba28a8ec5a
GLADY-PC\Gladson <GladsonAntony91@gmail.com>
2021-09-27 23:33:04 +0530

    Updated Dependencies. URL to use https

--- src/test/java/tests/Test01_ABTesting.java
+++ src/test/java/tests/Test01_ABTesting.java
@@ -6,20 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Test01_ABTesting
-{
+public class Test01_ABTesting {
     @Test
-    public void test1_ABTesting() throws Exception
-    {
+    public void test1_ABTesting() throws Exception {
         WebDriverManager.chromedriver().arch32().setup();
         WebDriver webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
-        webDriver.get("http://the-internet.herokuapp.com/");
-        System.out.println("Page Title --> "+webDriver.getTitle());
+        webDriver.get("https://the-internet.herokuapp.com/");
+        System.out.println("Page Title --> " + webDriver.getTitle());
         webDriver.findElement(By.linkText("A/B Testing")).click();
-        System.out.println("\nPage Title -->"+webDriver.getTitle());
-        System.out.println("Page Header --> "+webDriver.findElement(By.xpath("//h3")).getText());
-        System.out.println("Page Content: \n" +webDriver.findElement(By.xpath("//p")).getText());
+        System.out.println("\nPage Title -->" + webDriver.getTitle());
+        System.out.println("Page Header --> " + webDriver.findElement(By.xpath("//h3")).getText());
+        System.out.println("Page Content: \n" + webDriver.findElement(By.xpath("//p")).getText());
         webDriver.quit();
     }
 
